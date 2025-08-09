261e60f4c4f8a93ad2b23cf06bec6dc024f13923
Darshana752 <malithdarshana2000@gmail.com>
2025-08-09 20:20:29 +0530

    forum back end

---
+++ backend/src/main/java/com/example/backend/UserController.java
@@ -0,0 +1,20 @@
+package com.example.backend;
+
+import org.springframework.web.bind.annotation.*;
+import com.example.backend.*;
+
+@CrossOrigin(origins = "http://localhost:3000") // Allow React frontend
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+  private final UserRepository repo;
+
+  public UserController(UserRepository repo) {
+    this.repo = repo;
+  }
+
+  @PostMapping
+  public Users createUser(@RequestBody Users user) {
+    return repo.save(user);
+  }
+}
