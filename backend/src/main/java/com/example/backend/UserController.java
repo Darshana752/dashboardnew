eb9ccf107fe570ba37e60b45463bbfd16200a377
Malith Darshana <malithdarshana2000@gmail.com>
2025-08-13 17:44:31 +0530

    dv

--- backend/src/main/java/com/example/backend/UserController.java
+++ backend/src/main/java/com/example/backend/UserController.java
@@ -3,7 +3,7 @@ package com.example.backend;
 import org.springframework.web.bind.annotation.*;
 import com.example.backend.*;
 
-@CrossOrigin(origins = "http://localhost:3000") // Allow React frontend
+@CrossOrigin(origins = "http://localhost:300") // Allow React frontend
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
