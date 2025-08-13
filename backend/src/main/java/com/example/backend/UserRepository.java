bfe3f239873cb692c145555f6d0d80d5c35b99e3
Malith Darshana <malithdarshana2000@gmail.com>
2025-08-13 17:41:14 +0530

    Delete backend directory

--- backend/src/main/java/com/example/backend/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend;
-
-import com.example.backend.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-
-}
